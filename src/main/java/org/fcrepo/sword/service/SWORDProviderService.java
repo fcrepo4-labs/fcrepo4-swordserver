338722ca2861ebe36f8bf20dc04b50c58a774498
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2015-09-21 21:58:19 +0200

    Fix license header formatting and checkstyle issues

--- src/main/java/org/fcrepo/sword/service/SWORDProviderService.java
+++ src/main/java/org/fcrepo/sword/service/SWORDProviderService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 DuraSpace, Inc.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -176,13 +176,15 @@ public class SWORDProviderService {
         return service;
     }
 
-    private void addWorkspaces(Service service, FedoraResource fedoraResource) {
+    private void addWorkspaces(final Service service, final FedoraResource fedoraResource) {
         try {
-            Value[] dcTitles = fedoraResource.getProperty("dc:title").getValues();
+            final Value[] dcTitles = fedoraResource.getProperty("dc:title").getValues();
             if (dcTitles.length > 0) {
                 try {
-                    String title = dcTitles[0].getString();
-                    if (!title.isEmpty()) service.addWorkspace(title);
+                    final String title = dcTitles[0].getString();
+                    if (!title.isEmpty()) {
+                        service.addWorkspace(title);
+                    }
                 } catch (IndexOutOfBoundsException | NullPointerException e) {
                     log.warn("Found workspace container without dc:title property: "
                             + fedoraResource.getPath());
