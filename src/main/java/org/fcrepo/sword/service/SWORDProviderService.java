2b6e0ec5c226e3a4bede051708af1c57857ebc5f
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2015-09-25 18:40:03 +0200

    Refactoring service class
    Reordering invocations and reusing Fedora Kernel API tools

--- src/main/java/org/fcrepo/sword/service/SWORDProviderService.java
+++ src/main/java/org/fcrepo/sword/service/SWORDProviderService.java
@@ -15,21 +15,22 @@
  */
 package org.fcrepo.sword.service;
 
-import com.hp.hpl.jena.rdf.model.Property;
 import org.apache.abdera.Abdera;
 import org.apache.abdera.model.Service;
 import org.fcrepo.http.commons.session.SessionFactory;
+import org.fcrepo.kernel.api.RdfLexicon;
 import org.fcrepo.kernel.api.models.Container;
 import org.fcrepo.kernel.api.models.FedoraResource;
 import org.fcrepo.kernel.api.services.ContainerService;
 import org.fcrepo.kernel.api.services.NodeService;
-import org.modeshape.jcr.api.NamespaceRegistry;
+import org.fcrepo.kernel.api.utils.NamespaceTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.jcr.NamespaceRegistry;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.Value;
@@ -40,24 +41,28 @@ import static java.util.Collections.emptyMap;
 import static org.fcrepo.kernel.modeshape.rdf.converters.PropertyConverter.getPropertyNameFromPredicate;
 
 /**
+ * Implements all protocol related methods to be executed via {@link org.fcrepo.sword.http.SWORDWebResource}
+ *
  * @author claussni
  */
 @Component
 public class SWORDProviderService {
 
-    public static final  String  NS_SWORD                 = "http://purl.org/net/sword/";
-    public static final  String  NS_SWORD_TERMS           = "http://purl.org/net/sword/terms/";
-    public static final  String  RDF_PREFIX               = "sword";
-    public static final  String  SWORD_ROOT_PATH          = "/sword";
-    public static final  String  SWORD_COLLECTIONS_PATH   = SWORD_ROOT_PATH + "/collections";
-    public static final  String  SWORD_WORKSPACES_PATH    = SWORD_ROOT_PATH + "/workspaces";
-    public static final  String  SWORD_VERSION            = "2.0";
-    public static final  Boolean SWORD_SUPPORTS_VERBOSE   = true;
-    public static final  Boolean SWORD_SUPPORTS_NOOP      = true;
-    public static final  Integer SWORD_MAX_UPLOAD_SIZE_KB = Integer.MAX_VALUE;
-    public static final  String  NS_FEDORA_DEFINITIONS    = "http://fedora.info/definitions/v4/config#";
-    private static final Logger  log                      = LoggerFactory.getLogger(SWORDProviderService.class);
-    private final        Abdera  abdera                   = new Abdera();
+    public static final String SWORD_ROOT_PATH        = "/sword";
+    public static final String SWORD_COLLECTIONS_PATH = SWORD_ROOT_PATH + "/collections";
+    public static final String SWORD_WORKSPACES_PATH  = SWORD_ROOT_PATH + "/workspaces";
+
+    private static final String  NS_SWORD                 = "http://purl.org/net/sword/";
+    private static final String  NS_SWORD_TERMS           = "http://purl.org/net/sword/terms/";
+    private static final String  RDF_PREFIX               = "sword";
+    private static final String  SWORD_ROOT_LABEL         = "SWORD root";
+    private static final String  SWORD_VERSION            = "2.0";
+    private static final Boolean SWORD_SUPPORTS_VERBOSE   = true;
+    private static final Boolean SWORD_SUPPORTS_NOOP      = true;
+    private static final Integer SWORD_MAX_UPLOAD_SIZE_KB = Integer.MAX_VALUE;
+
+    private static final Logger log    = LoggerFactory.getLogger(SWORDProviderService.class);
+    private final        Abdera abdera = new Abdera();
 
     @Autowired
     private SessionFactory sessionFactory;
@@ -68,94 +73,7 @@ public class SWORDProviderService {
     @Autowired
     private ContainerService containerService;
 
-    private Container workspacesContainer;
-    private Container collectionsContainer;
-
-    /**
-     * Service intitialization
-     *
-     * @throws RuntimeException if initialization failes.
-     */
-    @PostConstruct
-    private void init() {
-        final Session session = sessionFactory.getInternalSession();
-        registerNamespace(session);
-        final Container root = initializeContainer(session, SWORD_ROOT_PATH, "SWORD root");
-        ensureDefaultProperties(session, root);
-        workspacesContainer = initializeContainer(session, SWORD_WORKSPACES_PATH, "SWORD workspaces");
-        collectionsContainer = initializeContainer(session, SWORD_COLLECTIONS_PATH, "SWORD collections");
-    }
-
-    private void ensureDefaultProperties(final Session session, final Container root) {
-        try {
-            ensureProperty(session, root, NS_FEDORA_DEFINITIONS + "supportsVerbose", SWORD_SUPPORTS_VERBOSE);
-            ensureProperty(session, root, NS_FEDORA_DEFINITIONS + "supportsNoop", SWORD_SUPPORTS_NOOP);
-            ensureProperty(session, root, NS_FEDORA_DEFINITIONS + "maxUploadSizeInKb", SWORD_MAX_UPLOAD_SIZE_KB);
-            session.save();
-        } catch (RepositoryException e) {
-            throw new RuntimeException("Failed to initialize default properties", e);
-        }
-    }
-
-    private void ensureProperty(
-            final Session session,
-            final Container root,
-            final String name,
-            final Object defaultValue) {
-        try {
-            final String propertyName = getPropertyName(session, createProperty(name));
-            if (!root.getNode().hasProperty(propertyName)) {
-                root.getNode().setProperty(propertyName, String.valueOf(defaultValue));
-            }
-        } catch (RepositoryException e) {
-            throw new RuntimeException("Failed to initialize property " + name, e);
-        }
-    }
-
-    /**
-     * Get a property name for an RDF predicate
-     *
-     * @param session
-     * @param predicate
-     * @return property name from the given predicate
-     * @throws RepositoryException
-     */
-    private String getPropertyName(final Session session, final Property predicate)
-            throws RepositoryException {
-
-        final NamespaceRegistry namespaceRegistry =
-                (org.modeshape.jcr.api.NamespaceRegistry) session.getWorkspace().getNamespaceRegistry();
-        final Map<String, String> namespaceMapping = emptyMap();
-        return getPropertyNameFromPredicate(namespaceRegistry, predicate, namespaceMapping);
-    }
-
-    private Container initializeContainer(final Session session, final String path, final String label) {
-        try {
-            if (!nodeService.exists(session, path)) {
-                log.info("Initializing " + label + " container {}", path);
-                final Container c = containerService.findOrCreate(session, path);
-                session.save();
-                return c;
-            }
-        } catch (RepositoryException e) {
-            throw new RuntimeException("Failed to initialize " + label + " container", e);
-        }
-        return null;
-    }
-
-    private void registerNamespace(final Session session) {
-        try {
-            final NamespaceRegistry namespaceRegistry;
-            namespaceRegistry = (NamespaceRegistry) session.getWorkspace().getNamespaceRegistry();
-
-            // Register the sword namespace if it's not found
-            if (!namespaceRegistry.isRegisteredPrefix(RDF_PREFIX)) {
-                namespaceRegistry.registerNamespace(RDF_PREFIX, NS_SWORD);
-            }
-        } catch (RepositoryException e) {
-            throw new RuntimeException("Failed to register SWORD namespace", e);
-        }
-    }
+    private Container workspaces;
 
     /**
      * Build and return a SWORD service document.
@@ -165,14 +83,8 @@ public class SWORDProviderService {
     public Service serviceDocument() {
         final Service service = abdera.newService();
         service.addSimpleExtension(NS_SWORD_TERMS, "version", "sword", SWORD_VERSION);
-        service.addSimpleExtension(NS_SWORD_TERMS, "maxUploadSize", "sword",
-                String.valueOf(SWORD_MAX_UPLOAD_SIZE_KB));
-
-        workspacesContainer.getChildren().forEachRemaining(
-                fedoraResource -> {
-                    addWorkspaces(service, fedoraResource);
-                });
-
+        service.addSimpleExtension(NS_SWORD_TERMS, "maxUploadSize", "sword", String.valueOf(SWORD_MAX_UPLOAD_SIZE_KB));
+        workspaces.getChildren().forEachRemaining(fedoraResource -> addWorkspaces(service, fedoraResource));
         return service;
     }
 
@@ -186,13 +98,69 @@ public class SWORDProviderService {
                         service.addWorkspace(title);
                     }
                 } catch (IndexOutOfBoundsException | NullPointerException e) {
-                    log.warn("Found workspace container without dc:title property: "
-                            + fedoraResource.getPath());
+                    log.warn("Found workspace container without dc:title property: {}", fedoraResource.getPath());
                 }
             }
         } catch (RepositoryException e) {
-            log.warn("Found workspace container with invalid dc:title property: "
-                    + fedoraResource.getPath());
+            log.warn("Found workspace container with invalid dc:title property: {}", fedoraResource.getPath());
+        }
+    }
+
+    @PostConstruct
+    private void init() {
+        final Session           session           = sessionFactory.getInternalSession();
+        final NamespaceRegistry namespaceRegistry = NamespaceTools.getNamespaceRegistry(session);
+
+        ensureSwordNamespaceRegistration(namespaceRegistry);
+
+        final Container root = getContainer(session, SWORD_ROOT_PATH, SWORD_ROOT_LABEL);
+        ensureProperty(namespaceRegistry, root, RdfLexicon.FEDORA_CONFIG_NAMESPACE + "supportsVerbose", SWORD_SUPPORTS_VERBOSE);
+        ensureProperty(namespaceRegistry, root, RdfLexicon.FEDORA_CONFIG_NAMESPACE + "supportsNoop", SWORD_SUPPORTS_NOOP);
+        ensureProperty(namespaceRegistry, root, RdfLexicon.FEDORA_CONFIG_NAMESPACE + "maxUploadSizeInKb", SWORD_MAX_UPLOAD_SIZE_KB);
+
+        workspaces = getContainer(session, SWORD_WORKSPACES_PATH, "SWORD workspaces");
+        getContainer(session, SWORD_COLLECTIONS_PATH, "SWORD collections");
+    }
+
+    private void ensureSwordNamespaceRegistration(final NamespaceRegistry namespaceRegistry) {
+        try {
+            namespaceRegistry.registerNamespace(RDF_PREFIX, NS_SWORD);
+        } catch (RepositoryException e) {
+            throw new RuntimeException(
+                    String.format("Failed to register namespace %s:%s", RDF_PREFIX, NS_SWORD), e);
+        }
+    }
+
+    private Container getContainer(final Session session, final String path, final String label) {
+        try {
+            if (!nodeService.exists(session, path)) {
+                log.info("Initializing container `{}` at `{}`", label, path);
+                final Container c = containerService.findOrCreate(session, path);
+                session.save();
+                return c;
+            }
+        } catch (RepositoryException e) {
+            throw new RuntimeException("Failed to initialize container " + label, e);
+        }
+        return null;
+    }
+
+    private void ensureProperty(
+            final NamespaceRegistry namespaceRegistry,
+            final Container container,
+            final String name,
+            final Object defaultValue) {
+        try {
+            final Map<String, String> namespaceMapping = emptyMap();
+            final String propertyName = getPropertyNameFromPredicate(
+                    (org.modeshape.jcr.api.NamespaceRegistry) namespaceRegistry,
+                    createProperty(name),
+                    namespaceMapping);
+            if (!container.getNode().hasProperty(propertyName)) {
+                container.getNode().setProperty(propertyName, String.valueOf(defaultValue));
+            }
+        } catch (RepositoryException e) {
+            throw new RuntimeException("Failed to initialize property " + name, e);
         }
     }
 
